72e56b37db911636dad3df5d2e39014323e14057
Bingle Kruger <bingkruger@hotmail.co.za>
2024-09-03 14:36:08 +0000

    feat: Import Trusted JSON files to enclave + Update readme + Update manifest template

--- sgx-mvp/README.md
+++ sgx-mvp/README.md
@@ -17,3 +17,15 @@ make SGX=1 wasm-mvp
 ```
 
 To test with non-SGX Gramine instead, omit `SGX=1` in both commands.
+
+# Alternate Gramine use
+
+```sh
+# Run program with Gramine SGX
+gramine-sgx sgx-mvp
+
+# Run program without SGX
+gramine-direct sgx-mvp
+```
+
+Note that _gramine-sgx_ requires all files we use in `sgx.trusted_files`
--- sgx-mvp/src/main.rs
+++ sgx-mvp/src/main.rs
@@ -1,5 +1,69 @@
 extern crate wasmi_impl;
 
+use serde_json::Value;
+use std::fs::{self, File};
+use std::io::Read;
+use std::path::Path;
+
+static WASM_FILE_MEAN: &str = "bin/get_mean_wasm.wasm";
+
 fn main() {
     println!("[+] Enclave created successfully");
+
+    // Attempt to read the WASM file, and handle the error if it occurs
+    let binary_mean = match fs::read(WASM_FILE_MEAN) {
+        Ok(data) => {
+            println!("[+] Successfully read WASM file");
+            data
+        }    
+        Err(e) => {
+            eprintln!("[-] Failed to read WASM file: {}", e);
+            return;
+        }
+    };
+ 
+    // Construct the path to the JSON data and schema files.
+    let test_data_file_path = "test-data/1_test_data.json";
+    let test_schema_file_path = "test-data/1_test_schema.json";
+
+    // Read the JSON data and schema from their respective files.
+    let test_json_data = match read_json_from_file(&test_data_file_path) {
+        Ok(data) => {
+            println!("[+] Successfully read JSON data file");
+            // println!("{}", serde_json::to_string_pretty(&data).unwrap());
+            data
+        } 
+        Err(e) => {
+            eprintln!("[-] Error reading JSON data file: {}", e);
+            return;
+        }
+    };
+    
+    let test_json_schema = match read_json_from_file(&test_schema_file_path) {
+        Ok(data) => {
+            println!("[+] Successfully read JSON schema file");
+            // println!("{}", serde_json::to_string_pretty(&data).unwrap());
+            data
+        }
+        Err(e) => {
+            eprintln!("[-] Error reading JSON schema file: {}", e);
+            return;
+        }
+    };
+
+    // Serialize the JSON data and schema.
+    let test_serialized_data = serde_json::to_vec(&test_json_data).expect("Failed to serialize data");
+    let test_serialized_schema = serde_json::to_vec(&test_json_schema).expect("Failed to serialize schema");
+
+    // Execute the WASM code with the serialized data and schema.
+
+    println!("[+] Successfully ran enclave code");
+
+}
+
+fn read_json_from_file<P: AsRef<Path>>(path: P) -> Result<Value, serde_json::Error> {
+    let mut file = File::open(path).expect("Unable to open file");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("Unable to read file");
+    serde_json::from_str(&contents)
 }
