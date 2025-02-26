5c89a3dea579de857d2fe9ba41eef5b476a99c79
Bingle Kruger <bingle@deeplearning-cafe.com>
2025-02-26 15:18:48 +0200

    chore: Reset state

--- drt-manager/programs/drt-manager/src/lib.rs
+++ drt-manager/programs/drt-manager/src/lib.rs
@@ -25,7 +25,7 @@ use anchor_spl::{
 };
 use mpl_token_metadata::ID as METADATA_PROGRAM_ID;
 
-declare_id!("4oTGrdA78dp2ukY7RFtsMrEM9LyRpBMqHkrD813CJre4");
+declare_id!("57Xvfo4c22qpDHXoginPd42DKnqqNWVp14QwHNTRZfoD");
 
 /// The vault account holds tokens and is a PDA.
 #[account]
